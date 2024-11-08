b905a699115802b3bd16f7624c80b508b80f9a19
Rhys <rhyswoody@gmail.com>
2024-11-08 13:53:31 -0800

    add coco and inital animations

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -3,39 +3,113 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
 
 public class Player extends Entity {
     GamePanel gp;
     KeyHandler keyH;
 
+
+
     public Player(GamePanel gp, KeyHandler keyH) {
         this.gp = gp;
         this.keyH = keyH;
         setDefaultValues();
+        getPlayerImage();
     }
     public void setDefaultValues(){
         x = 100;
         y = 100;
         speed = 4;
+        direction = "down";
+    }
+
+    public void getPlayerImage(){
+        try{
+            up1 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("player/coco-sitting-1.png"), "up1 image not found"));
+            up2 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("player/coco-walking-forward-left-paw.png"), "up2 image not found"));
+            down1 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("player/coco-sitting-1.png"), "down1 image not found"));
+            down2 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("player/coco-sitting-1.png"), "down2 image not found"));
+            left1 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("player/coco-walking-left-1.png"), "left1 image not found"));
+            left2 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("player/coco-walking-left-2.png"), "left2 image not found"));
+            right1 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("player/coco-walking-left-1.png"), "right1 image not found"));
+            right2 = ImageIO.read(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("player/coco-walking-left-2.png"), "right2 image not found"));
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
     }
     public void update(){
-        if(keyH.upPressed) {
-            y -= speed;
-        }
-        else if(keyH.downPressed) {
-            y += speed;
-        }
-        else if(keyH.leftPressed) {
-            x -= speed;
-        }
-        else if(keyH.rightPressed) {
-            x += speed;
-        }
+        if(keyH.downPressed == true || keyH.upPressed == true || keyH.leftPressed == true || keyH.rightPressed == true){
+            if(keyH.upPressed) {
+                direction = "up";
+                y -= speed;
+            }
+            else if(keyH.downPressed) {
+                direction = "down";
+                y += speed;
+            }
+            else if(keyH.leftPressed) {
+                direction = "left";
+                x -= speed;
+            }
+            else if(keyH.rightPressed) {
+                direction = "right";
+                x += speed;
+            }
 
+            spriteCounter++;
+            if(spriteCounter > 12){
+                if(spriteNum == 1){
+                    spriteNum = 2;
+                } else if (spriteNum == 2){
+                    spriteNum = 1;
+                }
+                spriteCounter = 0;
+            }
+        }
     }
     public void draw(Graphics2D g2){
-        g2.setColor(Color.WHITE);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+//        g2.setColor(Color.WHITE);
+//        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+        BufferedImage image = null;
+        switch(direction) {
+            case "up":
+                if(spriteNum == 1) {
+                    image = up1;
+                }
+                if(spriteNum == 2) {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if(spriteNum == 1) {
+                    image = down1;
+                }
+                if(spriteNum == 2) {
+                    image = down2;
+                }
+                break;
+            case "left":
+                if(spriteNum == 1) {
+                    image = left1;
+                }
+                if(spriteNum == 2) {
+                    image = left2;
+                }
+                break;
+            case "right":
+                if(spriteNum == 1) {
+                    image = right1;
+                }
+                if(spriteNum == 2) {
+                    image = right2;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
     }
 }
